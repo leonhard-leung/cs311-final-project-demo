636a25ce1dec4a24cb1cd8562dd155c51a2f8352
MARIUS GLENN NONATO <2232731@slu.edu.ph>
2024-10-23 21:03:36 +0800

    pushed codes in marius directory

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 mod hannah { pub mod documentation; }
 mod jerwin { pub mod refactor; }
 mod leonhard { pub mod testing; }
-mod marius { pub mod code_regeneration;}
+mod marius {
+    pub mod code_regeneration;
+    pub mod code_gen_derive;
+    pub mod code_gen_create_structs;
+    pub mod code_gen_traits_and_structs;
+}
 mod sanchie {pub mod debugging;}
 mod stephen { pub mod code_editing; }
 
@@ -10,6 +15,9 @@ fn main() {
     jerwin::refactor::refactor();
     leonhard::testing::testing();
     marius::code_regeneration::code_generation();
+    // marius::code_gen_derive::main();
+    // marius::code_gen_create_structs::main();
+    // marius::code_gen_traits_and_structs::main();
     sanchie::debugging::debugging();
     stephen::code_editing::code_editing();
 }
